83e770fbd3e0749473c3db874b28637e315b40f7
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-06-15 10:23:25 +0100

    Add files via upload

---
+++ C Primer (5ᵗʰ) 2012/Ch3/multiArray.cpp
@@ -0,0 +1,10 @@
+//Multidimensional Array
+//In a two dimensional array the 1st dimension is the row and the second is the column
+
+int ia[3][4] = {
+{0, 1, 2, 3}, 	// row[0]
+{4, 5, 6, 7},	// row[1]
+{8, 9, 10, 11}	// row[2]
+};
+
+
